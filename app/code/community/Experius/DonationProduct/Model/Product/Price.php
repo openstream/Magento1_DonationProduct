6c9404657983b73a1c98d909a33478dd2fb7b569
Derrick Heesbeen <derrick@experius.nl>
2017-11-15 09:37:28 +0000

    [TASK] donation product type, first attempt

--- app/code/community/Experius/DonationProduct/Model/Product/Price.php
+++ app/code/community/Experius/DonationProduct/Model/Product/Price.php
@@ -6,4 +6,19 @@ class Experius_DonationProduct_Model_Product_Price extends Mage_Catalog_Model_Pr
     {
         return (float)(time() / 100);
     }
+
+    /**
+     * Retrieve product final price
+     *
+     * @param float|null $qty
+     * @param Mage_Catalog_Model_Product $product
+     * @return float
+     */
+    public function getFinalPrice($qty = null, $product)
+    {
+        //$buyRequest = $product->getCustomOption('info_buyRequest');
+        //$product->setFinalPrice($finalPrice);
+
+        return '0.01';
+    }
 }
